10dd37ba733cc1aa4e70f3f1e49f2192e067b6ec
Ben Smyth <jbensmyth@gmail.com>
2018-06-20 09:42:23 -0400

    Create clock objects

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ const timer = document.getElementById("timer");
 
 window.onload = () => {timer.textContent = "25:00"};
 
-let workTime = 10;
-let breakTime = 10;
-
 function padded(a) {
 	if (a < 10) {
 		a = `0${a}`;
@@ -18,39 +15,30 @@ function padded(a) {
 	return a;
 }
 
+function updateTime() {
+	this.time--;
+	var seconds = this.time % 60;
+	var minutes = Math.floor((this.time / 60) % 60);
+	timer.textContent = `${padded(minutes)}:${padded(seconds)}`;
+	if (this.time === 0) clearInterval(interval);
+}
 
-// Need to work on timer functions.
+let workClock = {
+	"time": 1500,
+	"updateTime": updateTime
+}
 
-// function startWorkTimer(time) {
-// 	workTime--;
-// 	let seconds = workTime % 60;
-// 	let minutes = Math.floor((workTime / 60) % 60);
-// 	timer.textContent = `${padded(minutes)}:${padded(seconds)}`;
-
-//  	if (workTime == 0) {
-//  		clearInterval(startWorkTimer);
-//  		setInterval(startBreakTimer, 1000);
-//  	}
-// }
-
-// function startBreakTimer(time) {
-// 	breakTime--;
-// 	let seconds = breakTime % 60;
-// 	let minutes = Math.floor((breakTime / 60) % 60);
-// 	timer.textContent = `${padded(minutes)}:${padded(seconds)}`;
-
-//  	if (breakTime == 0) {
-//  		clearInterval(startBreakTimer);
-//  		setInterval(startWorkTimer, 1000);
-//  	}
-// }
+let breakClock = {
+	"time": 300,
+	"updateTime": updateTime
+}
 
 function startTimer() {
-
+	var interval = setInterval(workClock.updateTime(), 1000);
 }
 
 function pauseTimer() {
-
+	clearInterval(interval);
 }
 
 function stopTimer() {
